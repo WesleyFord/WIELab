37eacda98e0dcb10ea7e1d104db87e92b8adab92
milenamaul <milena.maul@kabelmail.de>
2020-05-31 18:07:26 +0200

    files added

---
+++ assignment07/resources/views/product/create.blade.php
@@ -0,0 +1,21 @@
+<form action="{{route('products.store')}}" method="POST">
+    @csrf
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                <textarea class="form-control" style="height:150px" name="price" placeholder="Detail"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+</form>
+@endsection
---
+++ assignment07/resources/views/product/show.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {{$product->name}}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Price:</strong>
+            {{$product->price}}
+        </div>
+    </div>
+</div>
