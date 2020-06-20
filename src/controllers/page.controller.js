2ac832c3a4a78e832aa251c71b83c467a2b8edb6
Enes Varcan <enesvarcan@gmail.com>
2020-06-20 19:16:07 +0200

    Fix unauthorized users accessing edit post page

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -183,6 +183,9 @@ exports.renderPostEdit = (req, res, next) => {
 
     //Post needed
     // -req.post
+
+    if(!req.post.userId.equals(req.user_id)) return res.redirect('/')
+    
     return res.render('edit-post', {user: req.user, profile:req.user.profile, post: req.post})
 
 }
